be9a9299a8340cd695710ccf5d8207cf0e4db354
ccmansilla <carloscmansilla@gmail.com>
2022-07-04 03:36:16 -0700

    modifico pages

--- pages.php
+++ pages.php
@@ -6,22 +6,22 @@
     <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Proyecto Codo a Codo</h5>
+      <p class="card-text">Pagina trabajo final frontend del curso de Fullstack Php del programa Codo a Codo de la Ciudad de Buenos Aires.</p>
+      <a href="https://ccmansilla.github.io/ciudad_bsas/" class="btn btn-primary" target="_blank">Abrir Pagina</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="img/code.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Pagina</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
   <div class="card" style="width: 18rem;">
-    <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
+    <img src="img/code.png" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Portfolio Carlos Mansilla</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  <div class="card" style="width: 18rem;">
-    <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Portfolio Carlos Mansilla</h5>
+      <h5 class="card-title">Pagina</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
@@ -30,7 +30,7 @@
 </div>
 <div class="tarjetas">
   <div class="card" style="width: 18rem;">
-    <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
+    <img src="img/code.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Proyecto Codo a Codo</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -38,17 +38,17 @@
     </div>
   </div>
   <div class="card" style="width: 18rem;">
-    <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
+    <img src="img/code.png" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Portfolio Carlos Mansilla</h5>
+      <h5 class="card-title">Pagina</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
   <div class="card" style="width: 18rem;">
-    <img src="img/codo_a_codo.png" class="card-img-top" alt="...">
+    <img src="img/code.png" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Portfolio Carlos Mansilla</h5>
+      <h5 class="card-title">Pagina</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
