a4f68892c2d239c7b4a570c3c315e1890f0a6934
ccmansilla <carloscmansilla@gmail.com>
2022-07-15 03:13:55 -0700

    database postgresql table proyectos

---
+++ db/proyectos_postgresql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE proyectos (
+  id serial PRIMARY KEY,
+  nombre varchar(500) UNIQUE NOT NULL,
+  imagen varchar(500) NOT NULL,
+  descripcion text NOT NULL,
+  url varchar(500) NOT NULL
+);
+
+
+
