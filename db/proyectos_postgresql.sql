241dc8fe27742f4dfed969492171f564ef32f8ba
ccmansilla <carloscmansilla@gmail.com>
2022-07-16 04:10:29 -0700

    modifico tabla proyectos para postgresql

--- db/proyectos_postgresql.sql
+++ db/proyectos_postgresql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE proyectos (
   id serial PRIMARY KEY,
-  nombre varchar(500) UNIQUE NOT NULL,
+  nombre varchar(500) NOT NULL,
   imagen varchar(500) NOT NULL,
   descripcion text NOT NULL,
   url varchar(500) NOT NULL
